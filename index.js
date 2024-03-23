2866d657ce78e863d66c104ae03bf758551a7351
nimrod njau <nimrodnjau@gmail.com>
2024-03-23 23:15:07 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// code your solution here
+function saturdayFun(activity="roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+}
+
+let mondayWork = function(activity="go to the office") {
+    return `This Monday, I will ${activity}.`
+  }
+  let wrapAdjective = function(style="*") {
+    return function(hum="special") {
+      return `You are ${style}${hum}${style}!`
+    }
+  }
+  const encouragingPromptFunction = wrapAdjective("!!!")
+  wrapAdjective("%")("a dedicated programmer")
